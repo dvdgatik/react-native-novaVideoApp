22f9fbc91aee946fb3b56209e3c806d10415c396
David Gatica <dvdgatic@gmail.com>
2021-04-18 00:41:20 -0500

    Estructura de recomendaciones composición de componentes

--- src/videos/containers/suggestion-list.js
+++ src/videos/containers/suggestion-list.js
@@ -3,32 +3,33 @@ import {
 	FlatList,
 	Text
 } from 'react-native';
+import Layout from '../components/suggestion-list-layout';
 class SuggestionList extends Component {
 	render() {
 		const list = [
 			{
-				title: 'David',
+				title: 'Titulo',
 				key: '1'
 			},
 			{
-				title: 'Gatica',
+				title: 'Otro Titulo',
 				key: '2'
 			}
 
 		]
 		return(
-
-			<FlatList
+			<Layout title='Recomendado para ti'>
+				<FlatList
 				data={list}
 				renderItem={({item})=> <Text>{item.title}</Text>}
-			>
-			
-			{/*data: puedes pasarle una lista o un array
-				key: id para evitar rerenders  y optimizar gracias a esta propiedad
-				renderItem: recibe una función y a su vez esta funcion puede renderizar un componente
-				por defecto renderiza toda la lista pero solo puedes recibir el item que esta dentro
-			*/}	
-			</FlatList>
+				>
+				{/*data: puedes pasarle una lista o un array
+					key: id para evitar rerenders  y optimizar gracias a esta propiedad
+					renderItem: recibe una función y a su vez esta funcion puede renderizar un componente
+					por defecto renderiza toda la lista pero solo puedes recibir el item que esta dentro
+				*/}	
+				</FlatList>
+			</Layout>
 			)
 	}
 }
